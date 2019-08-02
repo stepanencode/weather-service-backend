9449ec502223868e223680e2a0ed0a910cba4f3d
Oksana Stepanenko <stepanencode@gmail.com>
2019-08-02 10:51:08 +0300

    changed search model

--- models/search.model.js
+++ models/search.model.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
+const SearchItemSchema = new mongoose.Schema({
+    dateString: String,
+    temperature: String,
+    weatherText: String,
+    weatherIcon: String
+});
+
 const SearchSchema = new mongoose.Schema({
-  timestamp: {type: String, required: true},
-  city: {type: String, required: true},
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+    city: {type: String, required: true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    items: [SearchItemSchema]
+},{
+    timestamps: true
 });
 
 const Search = mongoose.model('Search', SearchSchema);
